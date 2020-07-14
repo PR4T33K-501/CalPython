8ed2612bb134002b8a239aea6cc263ac41207dcd
PR4T33K-501 <66904416+PR4T33K-501@users.noreply.github.com>
2020-07-14 14:46:36 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,3 +1,6 @@
+# This is a Basic Calculator
+print ("This is a super Basic Calculator\n")
+print ("Created By PR4T33K-501\n")
 def add(x, y):
     return x + y 
 def subtract(x, y):
@@ -6,14 +9,14 @@ def multiply(x, y):
     return x * y 
 def divide(x, y):
     return x / y
-print(" Choose calculation ") 
-print("1 +")
-print("2 -") 
-print("3 *") 
-print("4 /") 
-choice = input("please input your choice(1 or 2 or 3 or 4):") 
-num1 = int(input("please input a number: ")) 
-num2 = int(input("please input a number : "))
+print("       Choose calculation\n ") 
+print("(1) +")
+print("(2) -") 
+print("(3) *") 
+print("(4) /") 
+choice = input("please input your choice(1 or 2 or 3 or 4):\n") 
+num1 = int(input("Enter Thw First Number: ")) 
+num2 = int(input("Enter The Second Number: "))
 if choice == '1': print(num1,"+",num2,"=", add(num1,num2)) 
 elif choice == '2': print(num1,"-",num2,"=", subtract(num1,num2)) 
 elif choice == '3': print(num1,"*",num2,"=", multiply(num1,num2))
